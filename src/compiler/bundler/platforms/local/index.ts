a6df6c59a7c30e043ad20b185ae8db0ea51c798f
Evan Sellers <github@sellersew.com>
2024-04-01 15:49:07 -0400

    Fixed Vercel Resource Issue

--- src/compiler/bundler/platforms/local/index.ts
+++ src/compiler/bundler/platforms/local/index.ts
@@ -37,7 +37,7 @@ export class Local extends Bundler {
 
     private getBuffer() {
         return `
-            import { LocalServer, __internal__ as SherpaJS } from "${Files.unix(Files.join(Files.getRootDirectory(), "dist/src/environment/index.js"))}";
+            import { LocalServer, __internal__ as SherpaJS } from "${Files.unix(Files.join(Files.getRootDirectory(), "dist/index.js"))}";
 
             let portArg = process.argv[2];
             let port    = portArg && !isNaN(parseInt(portArg)) ? parseInt(portArg) : 3000;
--- src/compiler/bundler/platforms/vercel/index.ts
+++ src/compiler/bundler/platforms/vercel/index.ts
@@ -50,7 +50,7 @@ export class Vercel extends Bundler {
 
     private getBuffer(endpoint:Endpoint) {
         return `
-            import { __internal__ as SherpaJS } from "${Files.unix(Files.join(Files.getRootDirectory(), "dist/src/environment/index.js"))}";
+            import { __internal__ as SherpaJS } from "${Files.unix(Files.join(Files.getRootDirectory(), "dist/index.js"))}";
             import * as endpoint from "${Files.unix(endpoint.filepath)}";
             import import_context from "${Files.unix(endpoint.module.contextFilepath)}";
 
